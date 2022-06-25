dc931e7871c477320ed16a76deb20f49aec20b74
Mateus Souza <mateus_msouza@outlook.com>
2022-06-25 10:02:04 -0300

    oh yeah

--- estrutura-de-dados/prova-01/parser.py
+++ estrutura-de-dados/prova-01/parser.py
@@ -179,12 +179,12 @@ FORMATED_DATA = {
 def parse_register(register):
     if UNSORTED_DATA in register:
         parse_array_size(register, UNSORTED_DATA)
-    elif SORTED_DATA in register:
-        parse_array_size(register, SORTED_DATA)
     elif REVERSED_SORTED_DATA in register:
         parse_array_size(register, REVERSED_SORTED_DATA)
     elif ALMOST_SORTED_DATA in register:
         parse_array_size(register, ALMOST_SORTED_DATA)
+    elif SORTED_DATA in register:
+        parse_array_size(register, SORTED_DATA)
 
 def parse_array_size(register, tipo):
     if SIZE_10 in register:
@@ -240,27 +240,92 @@ def parse_sort(register, tipo, tamanho):
 
 
 def formated_print():
-    show_execuction_time('Conjunto não ordenado', UNSORTED_DATA)
-    show_execuction_time('Conjunto ordenado', SORTED_DATA)
-    show_execuction_time('Conjunto reversamente ordenado', REVERSED_SORTED_DATA)
-    show_execuction_time('Conjunto quase ordenado', ALMOST_SORTED_DATA)
+    
+    show_changes('Conjunto não ordenado', UNSORTED_DATA)
+    show_changes('Conjunto ordenado', SORTED_DATA)
+    show_changes('Conjunto reversamente ordenado', REVERSED_SORTED_DATA)
+    show_changes('Conjunto quase ordenado', ALMOST_SORTED_DATA)
 
 def show_execuction_time(conjunto, data_type):
     print(f'### {conjunto} - Tempo de Execução')
-    print('quick_sort_time: ', end='')
+    print('quick_sort_time = ', end='')
     print(FORMATED_DATA[data_type][QUICK_SORT][EXECUTION_TIME], sep=', ')
-    print('merge_sort_time: ', end='')
+    print('merge_sort_time = ', end='')
     print(FORMATED_DATA[data_type][MERGE_SORT][EXECUTION_TIME], sep=', ')
-    print('heap_sort_time: ', end='')
+    print('heap_sort_time = ', end='')
     print(FORMATED_DATA[data_type][HEAP_SORT][EXECUTION_TIME], sep=', ')
-    print('insertion_sort_time: ', end='')
+    print('insertion_sort_time = ', end='')
     print(FORMATED_DATA[data_type][INSERTION_SORT][EXECUTION_TIME], sep=', ')
-    print('selection_sort_time: ', end='')
+    print('selection_sort_time = ', end='')
     print(FORMATED_DATA[data_type][SELECTION_SORT][EXECUTION_TIME], sep=', ')
-    print('bubble_sort_time: ', end='')
+    print('bubble_sort_time = ', end='')
     print(FORMATED_DATA[data_type][BUBBLE_SORT][EXECUTION_TIME], sep=', ')
-    print('stooge_sort_time: ', end='')
+    print('stooge_sort_time = ', end='')
     print(FORMATED_DATA[data_type][STOOGE_SORT][EXECUTION_TIME], sep=', ')
+
+    print(f'plt.plot(array_size, quick_sort_time, label="{QUICK_SORT}")')
+    print(f'plt.plot(array_size, merge_sort_time, label="{MERGE_SORT}")')
+    print(f'plt.plot(array_size, heap_sort_time, label="{HEAP_SORT}")')
+    print(f'plt.plot(array_size, insertion_sort_time, label="{INSERTION_SORT}")')
+    print(f'plt.plot(array_size, selection_sort_time, label="{SELECTION_SORT}")')
+    print(f'plt.plot(array_size, bubble_sort_time, label="{BUBBLE_SORT}")')
+    print(f'plt.plot(array_size, stooge_sort_time, label="{STOOGE_SORT}")')
+    print('==================================================================')
+
+def show_comparisions(conjunto, data_type):
+    print(f'### {conjunto} - Comparações')
+    print('array_size = [10, 100, 1000, 10000, 100000]')
+    print('quick_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][QUICK_SORT][COMPARISIONS], sep=', ')
+    print('merge_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][MERGE_SORT][COMPARISIONS], sep=', ')
+    print('heap_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][HEAP_SORT][COMPARISIONS], sep=', ')
+    print('insertion_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][INSERTION_SORT][COMPARISIONS], sep=', ')
+    print('selection_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][SELECTION_SORT][COMPARISIONS], sep=', ')
+    print('bubble_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][BUBBLE_SORT][COMPARISIONS], sep=', ')
+    print('stooge_sort_comparisions = ', end='')
+    print(FORMATED_DATA[data_type][STOOGE_SORT][COMPARISIONS], sep=', ')
+
+    print(f'plt.plot(array_size, quick_sort_comparisions, label="{QUICK_SORT}")')
+    print(f'plt.plot(array_size, merge_sort_comparisions, label="{MERGE_SORT}")')
+    print(f'plt.plot(array_size, heap_sort_comparisions, label="{HEAP_SORT}")')
+    print(f'plt.plot(array_size, insertion_sort_comparisions, label="{INSERTION_SORT}")')
+    print(f'plt.plot(array_size, selection_sort_comparisions, label="{SELECTION_SORT}")')
+    print(f'plt.plot(array_size, bubble_sort_comparisions, label="{BUBBLE_SORT}")')
+    print(f'plt.plot(array_size, stooge_sort_comparisions, label="{STOOGE_SORT}")')
+    print('plt.xticks(array_size)')
+    print('==================================================================')
+
+def show_changes(conjunto, data_type):
+    print(f'### {conjunto} - Movimentações')
+    print('array_size = [10, 100, 1000, 10000, 100000]')
+    print('quick_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][QUICK_SORT][COMPARISIONS], sep=', ')
+    print('merge_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][MERGE_SORT][COMPARISIONS], sep=', ')
+    print('heap_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][HEAP_SORT][COMPARISIONS], sep=', ')
+    print('insertion_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][INSERTION_SORT][COMPARISIONS], sep=', ')
+    print('selection_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][SELECTION_SORT][COMPARISIONS], sep=', ')
+    print('bubble_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][BUBBLE_SORT][COMPARISIONS], sep=', ')
+    print('stooge_sort_changes = ', end='')
+    print(FORMATED_DATA[data_type][STOOGE_SORT][COMPARISIONS], sep=', ')
+
+    print(f'plt.plot(array_size, quick_sort_changes, label="{QUICK_SORT}")')
+    print(f'plt.plot(array_size, merge_sort_changes, label="{MERGE_SORT}")')
+    print(f'plt.plot(array_size, heap_sort_changes, label="{HEAP_SORT}")')
+    print(f'plt.plot(array_size, insertion_sort_changes, label="{INSERTION_SORT}")')
+    print(f'plt.plot(array_size, selection_sort_changes, label="{SELECTION_SORT}")')
+    print(f'plt.plot(array_size, bubble_sort_changes, label="{BUBBLE_SORT}")')
+    print(f'plt.plot(array_size, stooge_sort_changes, label="{STOOGE_SORT}")')
+    print('plt.xticks(array_size)')
     print('==================================================================')
 
 with open('dataset.txt', 'r') as dataset:
